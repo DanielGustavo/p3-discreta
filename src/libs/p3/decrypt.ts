9be5a40d4505e60f073ca0a2c23679c6414d9443
Daniel Gustavo <dg520589@gmail.com>
2024-10-25 00:43:12 -0300

    get shit done part 2

--- src/libs/p3/decrypt.ts
+++ src/libs/p3/decrypt.ts
@@ -1,43 +1,43 @@
 import { api } from "./api";
 
 type TDecryptRequest = {
-  formula: { a: number, b?: number, c?: number }
-  msg: string
-  degree: number
-}
+  formula: { a: number; b?: number; c?: number };
+  msg: string;
+  degree: number;
+};
 
 type TDecryptResponse = {
-  texto: string
-  textoEmNumero: number[]
-}
+  texto: string;
+  textoEmNumero: number[];
+};
 
 export type TParsedDecryptResponse = {
-  msg: string
-  msgInNumbers: number[]
-}
+  msg: string;
+  msgInNumbers: number[];
+};
 
 export async function decrypt({ formula, msg, degree }: TDecryptRequest) {
   const parsedFormula = {
     a: formula.a,
     b: formula.b ?? 0,
     c: formula.c ?? 0,
-  }
+  };
 
-  const key = `${degree}${msg.toLowerCase()}`
-  const msgInNumbersStringfied = localStorage.getItem(key)
-  const msgInNumbers = JSON.parse(msgInNumbersStringfied || '[]')
+  const key = `${degree}${msg.toLowerCase()}`;
+  const msgInNumbersStringfied = localStorage.getItem(key);
+  const msgInNumbers = JSON.parse(msgInNumbersStringfied || "[]");
 
-  const response = await api.post<TDecryptResponse>('/descriptografar', {
+  const response = await api.post<TDecryptResponse>("/descriptografar", {
     texto: msg,
     id: degree,
     textoEmNumero: msgInNumbers,
-    ...parsedFormula
-  })
+    ...parsedFormula,
+  });
 
   const parsedResponse: TParsedDecryptResponse = {
     msg: response.data.texto,
-    msgInNumbers: response.data.textoEmNumero
-  }
+    msgInNumbers: response.data.textoEmNumero,
+  };
 
-  return parsedResponse
+  return parsedResponse;
 }
--- src/libs/p3/encrypt.ts
+++ src/libs/p3/encrypt.ts
@@ -1,41 +1,72 @@
+import { toast } from "react-toastify";
 import { api } from "./api";
 
 type TEncryptRequest = {
-  formula: { a: number, b?: number, c?: number }
-  msg: string
-  degree: number
-}
+  formula: { a: number; b?: number; c?: number };
+  msg: string;
+  degree: number;
+};
 
 type TEncryptResponse = {
-  texto: string
-  textoEmNumero: number[]
-}
+  texto: string;
+  textoEmNumero: number[];
+};
 
 export type TParsedEncryptResponse = {
-  msg: string
-  msgInNumbers: number[]
+  msg: string;
+  msgInNumbers: number[];
+};
+
+function checkDuplicatedValues(vector: number[]) {
+  let hasDuplicated = false;
+
+  for (let i = 0; i < vector.length; i++) {
+    for (let j = 1; j < vector.length; j++) {
+      if (hasDuplicated) break;
+      if (i === j) continue;
+
+      hasDuplicated = vector[i] === vector[j];
+    }
+  }
+
+  return hasDuplicated;
 }
+
+let alphabet = "abcdefghijklmnopqrstuvwxyz.,";
 
 export async function encrypt({ formula, msg, degree }: TEncryptRequest) {
   const parsedFormula = {
     a: formula.a,
     b: formula.b ?? 0,
     c: formula.c ?? 0,
-  }
+  };
 
-  const response = await api.post<TEncryptResponse>('/criptografar', {
+  const response = await api.post<TEncryptResponse>("/criptografar", {
     texto: msg,
     id: degree,
-    ...parsedFormula
-  })
+    ...parsedFormula,
+  });
 
   const parsedResponse: TParsedEncryptResponse = {
     msg: response.data.texto,
-    msgInNumbers: response.data.textoEmNumero
+    msgInNumbers: response.data.textoEmNumero,
+  };
+
+  if (degree === 2) {
+    const response2 = await api.post<TEncryptResponse>("/criptografar", {
+      texto: alphabet,
+      id: degree,
+      ...parsedFormula,
+    });
+
+    if (checkDuplicatedValues(response2.data.textoEmNumero)) {
+      toast("Chave de criptografia incompatível", { type: "error" });
+      parsedResponse.msg = "";
+    }
   }
 
-  const key = `${degree}${parsedResponse.msg.toLowerCase()}`
-  localStorage.setItem(key, JSON.stringify(parsedResponse.msgInNumbers))
+  const key = `${degree}${parsedResponse.msg.toLowerCase()}`;
+  localStorage.setItem(key, JSON.stringify(parsedResponse.msgInNumbers));
 
-  return parsedResponse
+  return parsedResponse;
 }
